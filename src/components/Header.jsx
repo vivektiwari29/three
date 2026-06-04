75f22ff919afa588a224975d1578bdc19572d52d
Vivek Tiwari <2878357@tcs.com>
2026-06-04 15:40:38 +0530

    third change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,7 +13,7 @@ const Header = () => {
         variant={'unstyled'}
         color={'white'}
         >
-            <Link to = '/'>Homie</Link>
+            <Link to = '/'>HomeSickness</Link>
         </Button>
 
 
